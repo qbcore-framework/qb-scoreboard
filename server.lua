4e40f7c038bae0bcc9b03c1873bf93ed30fb10e8
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-11-10 01:39:19 -0600

    Count fix

--- server.lua
+++ server.lua
@@ -1,10 +1,7 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local TotalPlayers = 0
-local PoliceCount = 0
-local AmbulanceCount = 0
-local players = {}
 
 QBCore.Functions.CreateCallback('qb-scoreboard:server:GetCurrentPlayers', function(source, cb)
+    local TotalPlayers = 0
     for k, v in pairs(QBCore.Functions.GetPlayers()) do
         TotalPlayers = TotalPlayers + 1
     end
@@ -12,6 +9,8 @@ QBCore.Functions.CreateCallback('qb-scoreboard:server:GetCurrentPlayers', functi
 end)
 
 QBCore.Functions.CreateCallback('qb-scoreboard:server:GetActivity', function(source, cb)
+    local PoliceCount = 0
+    local AmbulanceCount = 0
     for k, v in pairs(QBCore.Functions.GetQBPlayers()) do
         if v.PlayerData.job.name == "police" and v.PlayerData.job.onduty then
             PoliceCount = PoliceCount + 1
@@ -29,6 +28,7 @@ QBCore.Functions.CreateCallback('qb-scoreboard:server:GetConfig', function(sourc
 end)
 
 QBCore.Functions.CreateCallback('qb-scoreboard:server:GetPlayersArrays', function(source, cb)
+    local players = {}
     for k, v in pairs(QBCore.Functions.GetQBPlayers()) do
         players[v.PlayerData.source] = {}
         players[v.PlayerData.source].permission = QBCore.Functions.IsOptin(v.PlayerData.source)
