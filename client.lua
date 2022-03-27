251427b282a3d104ca6874f3d43f24c4975a5069
P Gergő <69343477+Rav3n95@users.noreply.github.com>
2022-03-27 12:28:33 +0200

    Optimized while loop, implamented QbCore Functions

--- client.lua
+++ client.lua
@@ -2,56 +2,6 @@ local QBCore = exports['qb-core']:GetCoreObject()
 local scoreboardOpen = false
 local PlayerOptin = {}
 
--- Functions
-
-local function DrawText3D(x, y, z, text)
-	SetTextScale(0.35, 0.35)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(true)
-    AddTextComponentString(text)
-    SetDrawOrigin(x,y,z, 0)
-    DrawText(0.0, 0.0)
-    local factor = (string.len(text)) / 370
-    DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
-    ClearDrawOrigin()
-end
-
-local function GetPlayers()
-    local players = {}
-    for _, player in ipairs(GetActivePlayers()) do
-        local ped = GetPlayerPed(player)
-        if DoesEntityExist(ped) then
-            players[#players+1] = player
-        end
-    end
-    return players
-end
-
-local function GetPlayersFromCoords(coords, distance)
-    local players = GetPlayers()
-    local closePlayers = {}
-
-    if coords == nil then
-		coords = GetEntityCoords(PlayerPedId())
-    end
-    if distance == nil then
-        distance = 5.0
-    end
-    for _, player in pairs(players) do
-		local target = GetPlayerPed(player)
-		local targetCoords = GetEntityCoords(target)
-		local targetdistance = #(targetCoords - vector3(coords.x, coords.y, coords.z))
-		if targetdistance <= distance then
-            closePlayers[#closePlayers+1] = player
-		end
-    end
-
-    return closePlayers
-end
-
 -- Events
 
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
@@ -99,17 +49,21 @@ RegisterKeyMapping('scoreboard', 'Open Scoreboard', 'keyboard', Config.OpenKey)
 -- Threads
 
 CreateThread(function()
+    local loop = 1000
     while true do
         if scoreboardOpen then
-            for _, player in pairs(GetPlayersFromCoords(GetEntityCoords(PlayerPedId()), 10.0)) do
+            for _, player in pairs(QBCore.Functions.GetPlayersFromCoords(GetEntityCoords(PlayerPedId()), 10.0)) do
                 local PlayerId = GetPlayerServerId(player)
                 local PlayerPed = GetPlayerPed(player)
                 local PlayerCoords = GetEntityCoords(PlayerPed)
                 if Config.ShowIDforALL or PlayerOptin[PlayerId].permission then
-                    DrawText3D(PlayerCoords.x, PlayerCoords.y, PlayerCoords.z + 1.0, '['..PlayerId..']')
+                    QBCore.Functions.DrawText3D(PlayerCoords.x, PlayerCoords.y, PlayerCoords.z + 1.0, '['..PlayerId..']')
                 end
             end
+            loop = 0
+        else
+            loop = 1000
         end
-        Wait(5)
+        Wait(loop)
     end
 end)
