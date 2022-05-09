5ddc3bb6cea5c8750488351fa38e2bba19a314ae
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-05-09 19:12:16 +0200

    refactor(server): performance

--- server.lua
+++ server.lua
@@ -1,39 +1,27 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-QBCore.Functions.CreateCallback('qb-scoreboard:server:GetCurrentPlayers', function(source, cb)
-    local TotalPlayers = 0
-    for k, v in pairs(QBCore.Functions.GetPlayers()) do
-        TotalPlayers = TotalPlayers + 1
-    end
-    cb(TotalPlayers)
-end)
-
-QBCore.Functions.CreateCallback('qb-scoreboard:server:GetActivity', function(source, cb)
-    local PoliceCount = 0
-    local AmbulanceCount = 0
-    for k, v in pairs(QBCore.Functions.GetQBPlayers()) do
-        if v.PlayerData.job.name == "police" and v.PlayerData.job.onduty then
-            PoliceCount = PoliceCount + 1
-        end
-
-        if v.PlayerData.job.name == "ambulance" and v.PlayerData.job.onduty then
-            AmbulanceCount = AmbulanceCount + 1
-        end
-    end
-    cb(PoliceCount, AmbulanceCount)
-end)
-
-QBCore.Functions.CreateCallback('qb-scoreboard:server:GetConfig', function(source, cb)
+QBCore.Functions.CreateCallback('qb-scoreboard:server:GetConfig', function(_, cb)
     cb(Config.IllegalActions)
 end)
 
-QBCore.Functions.CreateCallback('qb-scoreboard:server:GetPlayersArrays', function(source, cb)
+QBCore.Functions.CreateCallback('qb-scoreboard:server:GetScoreboardData', function(_, cb)
+    local totalPlayers = 0
+    local policeCount = 0
     local players = {}
-    for k, v in pairs(QBCore.Functions.GetQBPlayers()) do
-        players[v.PlayerData.source] = {}
-        players[v.PlayerData.source].permission = QBCore.Functions.IsOptin(v.PlayerData.source)
+
+    for _, v in pairs(QBCore.Functions.GetQBPlayers()) do
+        if v then
+            totalPlayers += 1
+
+            if v.PlayerData.job.name == "police" and v.PlayerData.job.onduty then
+                policeCount += 1
+            end
+
+            players[v.PlayerData.source] = {}
+            players[v.PlayerData.source].optin = QBCore.Functions.IsOptin(v.PlayerData.source)
+        end
     end
-    cb(players)
+    cb(totalPlayers, policeCount, players)
 end)
 
 RegisterNetEvent('qb-scoreboard:server:SetActivityBusy', function(activity, bool)
